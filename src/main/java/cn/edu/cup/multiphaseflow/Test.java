50d1be8f05d8434c751897690af6a2486f519073
tanyao <1029681537@qq.com>
2019-11-01 22:42:29 +0800

    Initialize the maven project!

---
+++ src/main/java/cn/edu/cup/multiphaseflow/Test.java
@@ -0,0 +1,11 @@
+package cn.edu.cup.multiphaseflow;
+
+/**
+ * @author tanyao
+ * @date 2019/11/1
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
